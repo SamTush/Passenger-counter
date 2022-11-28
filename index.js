23eba1d0ebb1856621e7761e12f29a27043492bc
samson <samo4421@icloud.com>
2022-11-28 18:57:05 +0300

    Final web app

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 
 let count = 0; 
+let dash = " - ";
+let space = " ";
 
 function increment() { 
-    count = count + 1;
+    count += 1;
     countEl.innerText = count;
  }
 
  function save() {
-     count = 0;
-     countEl.innerText = count;
+     saveEl.innerText += (space + count + dash);
  }
 
+ function restart() {
+    count = 0;
+    countEl.innerText = count;
+    saveEl.innerText = "Previous enteries: "
+ }
 
 
 
